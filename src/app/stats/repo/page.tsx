189d7395b320edc68dac961a533fcb525b523a65
Nitish <tnitish654@gmail.com>
2024-01-21 04:35:24 +0800

    ⚡ Made card look more glassy

--- src/app/stats/repo/page.tsx
+++ src/app/stats/repo/page.tsx
@@ -13,14 +13,14 @@ const RepoPage = () => {
             </div>
             <Card className="col-span-4 row-span-4 h-full w-full ">huh</Card>
             <div className="col-span-5 row-span-2 h-full">
-                <div className="grid h-full grid-cols-4 grid-rows-2 gap-4">
-                    <Card className="col-span-1 row-span-1 h-full w-full ">
+                <div className="grid h-full grid-cols-6 grid-rows-2 gap-4">
+                    <Card className="col-span-2 row-span-1 h-full w-full ">
                         huh
                     </Card>
-                    <Card className="col-span-3 row-span-1 h-full w-full ">
+                    <Card className="col-span-4 row-span-1 h-full w-full ">
                         huh
                     </Card>
-                    <Card className="col-span-4 row-span-1 row-start-2 h-full w-full ">
+                    <Card className="col-span-6 row-span-1 row-start-2 h-full w-full ">
                         huh
                     </Card>
                 </div>
