66b314c874f07089e334a4417da832f6ff99d4bd
Putt <57494734+PuttTim@users.noreply.github.com>
2024-01-21 06:44:25 +0800

    ✨ Added fetching for every request we need from GitHub

--- src/app/parse/route.ts
+++ src/app/parse/route.ts
@@ -1,49 +1,207 @@
+import { FormattedRepos } from "@/types/githubResponses"
 import { DateTime } from "luxon"
 import { NextResponse } from "next/server"
+import { flatExtMap, languageMapping } from "../utils/languageMapping"
+
+const year = 2023
+const yearStart = DateTime.fromObject({ year: year }, { zone: "utc" }).startOf(
+    "year",
+)
+const yearEnd = DateTime.fromObject({ year: year }, { zone: "utc" }).endOf(
+    "year",
+)
 
 export async function POST() {
-	const token = process.env.GH_TOKEN
-	let userRepoPage = 1
-	let userRepos: unknown[] = []
+    const token = process.env.GH_TOKEN
+    const username = process.env.USER!
 
-	const headers = { Authorization: `Bearer ${token}`, "X-GitHub-Api-Version": "2022-11-28" }
+    console.log(yearStart, yearEnd)
 
-	const options = {
-		headers,
-		next: {
-			revalidate: 86400,
-		},
-	}
+    const headers = {
+        Authorization: `Bearer ${token}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
 
-	while (true) {
-		const res = await fetch(
-			`https://api.github.com/user/repos?sort=pushed&per_page=100&page=${userRepoPage}`,
-			options
-		)
+    const options = {
+        headers,
+        next: {
+            revalidate: 86400,
+        },
+    }
 
-		const json = await res.json()
-		if (json.length === 0) break
-		userRepos = userRepos.concat(json)
-		userRepoPage++
-	}
+    const repos = await getReposData(options, username)
 
-	// @ts-expect-error
-	userRepos = userRepos.filter((repo) => DateTime.fromISO(repo.pushed_at).year >= 2023)
+    let issuesCount: number = 0
+    let prCount: number = 0
+    let abandonedCount: number = 0
+    let commitData = []
 
-	// @ts-expect-error
-	const repoNames = userRepos.map((repo) => ({ name: repo.full_name, year: repo.pushed_at }))
+    for (const repo of repos) {
+        abandonedCount += repo.isAbandoned ? 1 : 0
 
-	console.log(repoNames)
+        if (repo.has_issues) {
+            const { issuesCount: issues, prCount: pr } = await getIssuesData(
+                options,
+                repo.uri,
+                username,
+            )
 
-	// return Response.json({ repoNames })
-	return NextResponse.json({ repoNames }, { status: 200 })
+            issuesCount += issues
+            prCount += pr
+
+            //  += await getIssuesData(options, repo.uri, username!)
+        }
+
+        const commitUrls = await getCommitsListData(options, repo.uri, username)
+
+        for (const url of commitUrls) {
+            commitData.push(await getCommitData(options, url))
+        }
+    }
+
+    // return Response.json({ repoNames })
+    return NextResponse.json(
+        { issuesCount, prCount, abandonedCount, commitData },
+        { status: 200 },
+    )
 }
 
-const getCommitData = async (requestOptions: any, commitUrl: string) => {
-	const res = await fetch(commitUrl, requestOptions)
+const getReposData = async (requestOptions: any, username: string) => {
+    let page = 1
+    let userRepos: FormattedRepos[] = []
+
+    while (true) {
+        const res = await fetch(
+            `https://api.github.com/user/repos?sort=pushed&per_page=100&page=${page}`,
+            requestOptions,
+        )
+
+        // console.log(res)
+
+        const json = await res.json()
+        if (json.length === 0) break
+        userRepos = userRepos.concat(
+            json
+                .filter(
+                    (repo: any) =>
+                        DateTime.fromISO(repo.pushed_at).year >= 2023,
+                )
+                .map((repo: any) => {
+                    return {
+                        uri: repo.full_name,
+                        stargazers_count: repo.stargazers_count,
+                        language: repo.language,
+                        created_at: repo.created_at,
+                        isAbandoned:
+                            DateTime.fromISO(repo.pushed_at).diff(
+                                yearEnd,
+                                "months",
+                            ).months < -3 &&
+                            repo.owner.login.toLowerCase() === username,
+                        has_issues: repo.has_issues,
+                        // repoData: repo
+                    }
+                }),
+        )
+
+        if (json.length < 100) break
+        page++
+    }
+
+    return userRepos
 }
 
-// get issues, qparam (paginated): creator, state by all,
+const getIssuesData = async (
+    requestOptions: any,
+    repoUri: string,
+    username: string,
+) => {
+    let page = 1
+    let issuesCount = 0
+    let prCount = 0
+
+    while (true) {
+        const res = await fetch(
+            `https://api.github.com/repos/${repoUri}/issues?state=all&creator=${username}&per_page=100&page=${page}`,
+            requestOptions,
+        )
+
+        const json = await res.json()
+        if (json.length === 0) break
+
+        json.filter(
+            (issue: any) => DateTime.fromISO(issue.created_at).year === 2023,
+        ).reduce((acc: any, issue: any) => {
+            if (issue.node_id.split("_")[0] === "I") issuesCount++
+            else if (issue.node_id.split("_")[0] === "PR") prCount++
+        }, {})
+
+        if (json.length < 100) break
+        page++
+    }
+
+    return { issuesCount, prCount }
+}
+
+const getCommitsListData = async (
+    requestOptions: any,
+    repoUri: string,
+    username: string,
+) => {
+    let page = 1
+    let commitUrls: string[] = []
+
+    while (true) {
+        const res = await fetch(
+            `https://api.github.com/repos/${repoUri}/commits?author=${username}&since=${yearStart}&until=${yearEnd}&per_page=100&page=${page}`,
+            requestOptions,
+        )
+
+        const json = await res.json()
+        if (json.length === 0 || res.status !== 200) break
+
+        commitUrls = commitUrls.concat(json.map((commit: any) => commit.url))
+
+        if (json.length < 100) break
+        page++
+    }
+
+    console.log(commitUrls)
+
+    return commitUrls
+}
+
+const getCommitData = async (requestOptions: any, url: string) => {
+    const res = await fetch(url, requestOptions)
+
+    const json = await res.json()
+
+    const commitDateTime = DateTime.fromISO(json.commit.committer.date)
+
+    let commitData = []
+
+    for (const file of json.files) {
+        const ext = file.filename.split(".").at(-1)
+        if (flatExtMap.includes(ext)) {
+            commitData.push({
+                language: Object.keys(languageMapping).find(key =>
+                    // @ts-expect-error
+                    languageMapping[key].ext.includes(ext),
+                ),
+                additions: file.additions,
+            })
+        }
+    }
+
+    return {
+        message: json.commit.message,
+        weekdate: commitDateTime.weekdayLong,
+        hour: commitDateTime.hour,
+        deletions: json.stats.deletions,
+        additions: json.stats.additions,
+        files: commitData,
+    }
+}
 
 // get prs, qparam (paginated): sort by created, state by all
 
